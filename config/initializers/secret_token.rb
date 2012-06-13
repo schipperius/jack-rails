dcf11d29c6495b046ceb29e64ec2049acb38c5be
Your Name <you@example.com>
2012-06-13 17:03:36 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JackRails::Application.config.secret_token = 'a159d0b1835e13575996065279c800f983037af8f2fe9f6610328bf7829ed1430ca0326e08268188b7e4ae22beae4b7a042e7a57df8aaf0f4f886dc7de7131a1'
